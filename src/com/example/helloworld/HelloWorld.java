3cf7a68bb2a0f37c2d5dd3f533866bfe8391dd58
Luai Assori <lucaly74@gmail.com>
2026-09-05 00:17:48 -0500

    made a change in newBranch

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     void main() {
-        IO.println("test...");
+        IO.println("testing...");
         IO.println("Hello World");
     }
 }
