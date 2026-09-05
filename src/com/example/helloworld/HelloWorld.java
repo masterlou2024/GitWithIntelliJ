430e48a1a802f7042674600235e6e447f5a446a7
Luai Assori <lucaly74@gmail.com>
2026-09-05 00:16:45 -0500

    Revert "print statement added"
    This reverts commit 8bf9b89d6d175d6cebbdc1fd82d7a0af1dba7871.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     void main() {
-        IO.println("testing...");
+        IO.println("test...");
         IO.println("Hello World");
     }
 }
