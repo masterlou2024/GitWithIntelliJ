5fc9a6e76682273b6bfd6509615a09c3df25f05c
Luai Assori <lucaly74@gmail.com>
2026-09-06 01:29:20 -0500

    stashed changes added

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 public class HelloWorld {
     void main() {
         IO.println("testing...");
+
+        IO.println("testing stashing...");
+
         IO.println("Hello World");
     }
 }
